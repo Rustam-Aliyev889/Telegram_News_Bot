5ee35bf70f7281d85e1f0ee00670ace2f83ef69b
Rustam Aliyev <rustam.aliyev889@gmail.com>
2024-03-26 10:36:45 +0000

    func to summarize an article

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from newspaper import Article
 import requests
 import configparser
 
@@ -9,18 +10,30 @@ config.read('config.ini')
 NEWS_API_KEY = config['DEFAULT']['NEWS_API_KEY']
 TELEGRAM_TOKEN = config['DEFAULT']['TELEGRAM_TOKEN']
 
-# Use the API keys and tokens in your code
-print(NEWS_API_KEY)
-print(TELEGRAM_TOKEN)
-
 
 def fetch_news():
     url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={NEWS_API_KEY}'
+    # a GET request to the News API
     response = requests.get(url)
     data = response.json()
+    # The list of articles
     return data['articles']
+
+# Function to summarize an article given its URL
+def summarize_article(url):
+    # Creates an Article object
+    article = Article(url)
+    # Downloads the article's content
+    article.download()
+    # To parse the article's content
+    article.parse()
+    # Uses natural language processing to the article
+    article.nlp()
+    return article.summary
 
 if __name__ == '__main__':
     articles = fetch_news()
     for article in articles:
         print(article['title'])
+        print("Summary:", summarize_article(article['url']))
+
