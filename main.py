16f394c5ceff327194d416c7de55f9c786eb326f
Rustam Aliyev <rustam.aliyev889@gmail.com>
2024-03-26 14:06:36 +0000

    skips the articles without content

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import nltk
 
-# the 'punkt' tokenizer models from NLTK
-nltk.download('punkt')
+nltk.download('punkt')   # the 'punkt' tokenizer models from NLTK
 
 from telegram import Bot, Update, ParseMode
 from telegram.ext import Updater, CommandHandler, CallbackContext
@@ -9,33 +8,28 @@ from newspaper import Article
 import requests
 import configparser
 
-# Configuration file
-config = configparser.ConfigParser()
+
+config = configparser.ConfigParser()   # Configuration file
 config.read('config.ini')
 
-# NEWS_API_KEY and TELEGRAM_TOKEN
 NEWS_API_KEY = config['DEFAULT']['NEWS_API_KEY']
 TELEGRAM_TOKEN = config['DEFAULT']['TELEGRAM_TOKEN']
 
 
 def fetch_news():
     url = f'https://newsapi.org/v2/top-headlines?country=gb&apiKey={NEWS_API_KEY}'
-    # a GET request to the News API
-    response = requests.get(url)
+    response = requests.get(url)   # a GET request to the News API
     data = response.json()
-    # The list of articles
-    return data['articles']
+    return data['articles']   # The list of articles in JSON format
 
-# Function to summarize an article given its URL
-def summarize_article(url):
-    # Creates an Article object
-    article = Article(url)
-    # Downloads the article's content
+
+def summarize_article(url):  # Function to summarize an article given its URL
+    article = Article(url) # Creates an Article object
     article.download()
-    # To parse the article's content
     article.parse()
-    # Uses natural language processing to the article
-    article.nlp()
+    if not article.text:
+        return None
+    article.nlp() # Uses natural language processing to the article text
     return article.summary
 
 # Command handler for the '/start' command
@@ -46,9 +40,11 @@ def start(update: Update, context: CallbackContext) -> None:
 # Command handler for the '/news' command
 def post_news(update: Update, context: CallbackContext) -> None:
     articles = fetch_news()
-    # Iterates through each article and post its title, summary, and source
-    for article in articles:
+    
+    for article in articles:   # Iterates through each article and post its title, summary, and source
         summary = summarize_article(article['url'])
+        if summary is None:
+            continue
         # Sends a message with the article's title, summary, and source URL
         update.message.reply_text(
             f"{article['title']}\n\n{summary} \n\n Source: {article['url']}")
@@ -56,14 +52,10 @@ def post_news(update: Update, context: CallbackContext) -> None:
 
 if __name__ == '__main__':
     updater = Updater(token=TELEGRAM_TOKEN)
-    # Gets the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-    # To register the '/start' command handler
-    dispatcher.add_handler(CommandHandler('start', start))
-    # To register the '/news' command handler
-    dispatcher.add_handler(CommandHandler('news', post_news))
-    # Starts the Bot
-    updater.start_polling()
+    dispatcher = updater.dispatcher            # Gets the dispatcher to register handlers
+    dispatcher.add_handler(CommandHandler('start', start))       # To register the '/start' command handler
+    dispatcher.add_handler(CommandHandler('news', post_news))    # To register the '/news' command handler
+    updater.start_polling()      # Starts the Bot
     # Run the bot until you send a SIGINT (Ctrl+C) or the process receives a SIGTERM, SIGABRT, or SIGQUIT signal
     updater.idle()
 
