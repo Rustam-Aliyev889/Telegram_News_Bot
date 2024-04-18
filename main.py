429a1e5ad9847068232b970857b177351e1d97ba
Rustam Aliyev <rustam.aliyev889@gmail.com>
2024-04-18 12:43:05 +0100

    changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import nltk
+import nltk #NLTK (Natural Language Toolkit) is a Python for working with human language data.
 from telegram import Bot, Update, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from telegram.ext import Updater, CommandHandler, CallbackContext, JobQueue, CallbackQueryHandler
 from newspaper import Article
@@ -8,12 +8,12 @@ import logging
 from datetime import time, timedelta, datetime
 import pytz, telegram.error
 
-
+# Setting up logging configuration for troubleshooting
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-nltk.download('punkt')   # the 'punkt' tokenizer models from NLTK
+nltk.download('punkt')   # tokenizer for splitting text into individual sentences
 
 config = configparser.ConfigParser()   # Configuration file
 config.read('config.ini')
@@ -21,19 +21,23 @@ config.read('config.ini')
 NEWS_API_KEY = config['DEFAULT']['NEWS_API_KEY']
 TELEGRAM_TOKEN = config['DEFAULT']['TELEGRAM_TOKEN']
 
-
+# Gets news for NewsApi
 def fetch_news():
     url = f'https://newsapi.org/v2/top-headlines?country=gb&apiKey={NEWS_API_KEY}'
     response = requests.get(url)   # a GET request to the News API
     data = response.json()
     return data['articles']   # The list of articles in JSON format
 
+
+# News categories from NewsApi
 def fetch_category_news(category):
     url = f'https://newsapi.org/v2/top-headlines?country=gb&category={category}&apiKey={NEWS_API_KEY}'
     response = requests.get(url)
     data = response.json()
     return data['articles']
 
+
+# Hoposcope Api
 def fetch_horoscope(sign):
     url = f"http://sandipbgt.com/theastrologer/api/horoscope/{sign.lower()}/{'today'}"
     response = requests.get(url)
@@ -129,7 +133,7 @@ def post_category_news(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     category = query.data.split('_')[1]
     
-    articles = fetch_category_news(category)[:8]
+    articles = fetch_category_news(category)[:9]
     
     for article in articles:
         summary = summarize_article(article['url'])
@@ -137,11 +141,12 @@ def post_category_news(update: Update, context: CallbackContext) -> None:
             continue
         
         try:
-            message_text = f"{article['title']}\n\n{summary}\n\nRead in full here: {article['url']}"
+            message_text = f"<b>{article['title']}</b>\n\n{summary} \n\n <a href='{article['url']}'>Read in full here</a>"
             context.bot.send_message(
                 chat_id=query.message.chat_id,
                 text=message_text,
-                timeout=10
+                parse_mode=ParseMode.HTML,
+                timeout=15
             )
         except telegram.error.BadRequest as e:
             # Logs the error and skips to the next article
@@ -186,7 +191,7 @@ def select_sign(update: Update, context: CallbackContext) -> None:
     [InlineKeyboardButton("Aquarius ♒", callback_data='sign_aquarius')],
     [InlineKeyboardButton("Pisces ♓", callback_data='sign_pisces')],
 ]
-    
+    # Sends message to select horoscope sign
     query.message.reply_text(
         text="Please select your horoscope sign:",
         reply_markup=InlineKeyboardMarkup(sign_keyboard)
@@ -259,7 +264,7 @@ def post_news(update: Update, context: CallbackContext) -> None:
             text=message_text,
             parse_mode=ParseMode.HTML
         )
-
+    start(update, context)      # Returns to the main menu after displaying news
 
 def schedule_post_news(context: CallbackContext) -> None:
     logger.info("Executing schedule_post_news function...")
@@ -279,6 +284,7 @@ def schedule_post_news(context: CallbackContext) -> None:
             parse_mode=ParseMode.HTML,
             timeout=60
         )
+
 
 if __name__ == '__main__':
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True, request_kwargs={'read_timeout': 20, 'connect_timeout': 20})
